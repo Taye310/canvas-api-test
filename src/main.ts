4f2d422fdacfc2123f82482a9318f71015e848f5
Taye310 <hi838792022@163.com>
2017-02-20 16:22:37 +0800

    Finish Homework 2

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ window.onload = () => {
     var offset: number = 0;
     var stage = new DisplayObjectContainer();
 
+    // setInterval(() => {
+    //     stage.array = [];
+    //     context2D.clearRect(0, 0, 400, 400);
+    //     var img2 = new Bitmap("/pic.jpg");
+    //     img2.rotation = 45;
+    //     stage.addChild(img2);
+    //     stage.Draw(context2D);
+    // }, 100)
 
     setInterval(() => {
         offset++;
@@ -15,17 +23,16 @@ window.onload = () => {
         var tf1 = new TextField("hello");
         tf1.x = 10;
         tf1.y = offset;
-        tf1.localAlpha = 0.3;
+        tf1.localAlpha = 0.8;
         var tf2 = new TextField("world");
-        tf2.x = 35;
+        tf2.x = 25;
         tf2.y = offset;
         //tf2.scaleX=3;////////scale后移动速度也会增加 是否应该解决？？？ 
         var img1 = new Bitmap("/pic.jpg");
         img1.x = offset;
-        img1.y = 0;
-        img1.scaleY=2;
-        //img1.rotation=90;
-        
+        img1.scaleY = 2;
+        img1.rotation = 45;//unfinish
+
         stage.localAlpha = 0.6;
         stage.x = offset;
 
@@ -100,10 +107,11 @@ class TextField extends DisplayObject {
         this.globalAlpha = this.parent.globalAlpha * this.localAlpha;
         this.globalMat = math.matrixAppendMatrix(this.parent.globalMat, this.localMat);
         context.globalAlpha = this.globalAlpha;
-        context.scale(this.globalMat.a, this.globalMat.d);
+        context.setTransform(this.globalMat.a, this.globalMat.b, this.globalMat.c, this.globalMat.d, this.globalMat.tx, this.globalMat.ty);
         context.fillText(this.text, this.globalMat.tx, this.globalMat.ty);
+        //还原
         context.globalAlpha = 1;
-        context.scale(1/this.globalMat.a,1/this.globalMat.d);
+        context.setTransform(1, 0, 0, 1, 0, 0);
     }
 }
 
@@ -125,12 +133,10 @@ class Bitmap extends DisplayObject {
         console.log(this.globalMat.toString());
 
         context.globalAlpha = this.globalAlpha;
-        context.scale(this.globalMat.a, this.globalMat.d);
-        context.rotate(Math.asin(this.globalMat.b)*Math.PI/180);//unfinish
+        context.setTransform(this.globalMat.a, this.globalMat.b, this.globalMat.c, this.globalMat.d, this.globalMat.tx, this.globalMat.ty);
         context.drawImage(this.img, this.globalMat.tx, this.globalMat.ty, this.width, this.height);
         ////还原
         context.globalAlpha = 1;
-        context.scale(1/this.globalMat.a,1/this.globalMat.d);
-        context.rotate(-Math.asin(this.globalMat.b)*Math.PI/180);//unfinish
+        context.setTransform(1, 0, 0, 1, 0, 0);
     }
 }
